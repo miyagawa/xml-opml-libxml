9b9fc6bff6ac77b84203017611fba9451b2996f1
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-10-09 18:05:48 +0000

    use cmpthese and added sample data output

--- eg/benchmark.pl
+++ eg/benchmark.pl
@@ -2,7 +2,7 @@
 use strict;
 use lib "lib";
 use warnings;
-use Benchmark;
+use Benchmark qw(:all);
 use FindBin;
 
 use XML::OPML::LibXML;
@@ -11,7 +11,7 @@ use XML::OPML;
 my $file = $ARGV[0] || "presentation.opml";
 my $path = "$FindBin::Bin/../t/samples/$file";
 
-timethese 1000, {
+cmpthese 1000, {
     'XML::OPML' => \&xml_opml,
     'XML::OPML::LibXML' => \&xml_opml_libxml,
     'XML::OPML::LibXML walkdown' => \&xml_opml_libxml_walkdown,
@@ -32,3 +32,9 @@ sub xml_opml_libxml_walkdown {
     my $doc = XML::OPML::LibXML->new->parse_file($path);
     $doc->walkdown(sub { my $text = $_[0]->text; die });
 }
+
+__DATA__
+                             Rate XML::OPML XML::OPML::LibXML XML::OPML::LibXML walkdown
+XML::OPML                  85.9/s        --              -78%                       -84%
+XML::OPML::LibXML           389/s      353%                --                       -26%
+XML::OPML::LibXML walkdown  524/s      509%               35%                         --
